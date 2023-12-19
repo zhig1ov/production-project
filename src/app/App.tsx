01f1f8dfadcba6fa34212a1e3aadd4cd0a3d0a93
zhig1ov <zhiglov.artem@yandex.ru>
2023-12-19 21:35:44 +0500

    added FSD architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route, Link } from "react-router-dom";
-import { Suspense, useContext, useState } from "react";
+import { Suspense } from "react";
 import './styles/index.scss';
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Theme, ThemeContext } from './theme/ThemeContext';
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { AboutPage } from "pages/AboutPage/index";
+import { MainPage } from "pages/MainPage/index";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 
 
 
@@ -19,8 +18,8 @@ const {theme, toggleTheme} = useTheme()
       <Link to={'./about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />} />
-          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
 
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState, FC } from 'react';
-import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from './ThemeContext';
+import { ThemeContext, LOCAL_STORAGE_THEME_KEY, Theme } from '../lib/ThemeContext';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
 
@@ -7,9 +7,7 @@ const ThemeProvider: FC = ({children}) => {
 
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
-  const toggleTheme = () => {
-    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-  }
+
 
   const defaultProps = useMemo(() => ({
     theme: theme,
