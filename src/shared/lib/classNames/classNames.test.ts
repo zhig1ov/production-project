d639839ea77b0bf35ad1b7cefd900aeaf4a4b9a7
zhig1ov <zhiglov.artem@yandex.ru>
2023-12-25 00:52:41 +0500

    add jest, first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,45 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    // test('with only first class');
+    test('with only first class', () => {
+        const expected = 'mainClass';
+        expect(classNames('mainClass')).toBe(expected);
+    });
+
+    test('with additional', () => {
+        const expected = 'mainClass class1, class2';
+        expect(classNames('mainClass', {}, ['class1, class2']))
+            .toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'mainClass class1 class2 hovered scrollable';
+        expect(classNames(
+            'mainClass',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+
+    test('with only first class', () => {
+        const expected = 'mainClass class1 class2 hovered';
+        expect(classNames(
+            'mainClass',
+            { hovered: true, scrollable: false },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+
+    test('with only first class', () => {
+        const expected = 'mainClass class1 class2 hovered';
+        expect(classNames(
+            'mainClass',
+            { hovered: true, scrollable: undefined },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+});
