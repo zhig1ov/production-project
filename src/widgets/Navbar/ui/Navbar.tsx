48779d04e68c5a658c2009e7a6975dbc200ea70c
zhig1ov <zhiglov.artem@yandex.ru>
2024-01-03 17:08:14 +0500

    add translate for all files

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,28 +1,32 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink
-                data-testid="navbar"
-                to="./"
-                theme={AppLinkTheme.SECONDARY}
-                className={cls.mainLink}
-            >
-                Главная
-            </AppLink>
-            <AppLink
-                to="./about"
-                theme={AppLinkTheme.SECONDARY}
-            >
-                О сайте
-            </AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    data-testid="navbar"
+                    to="./"
+                    theme={AppLinkTheme.SECONDARY}
+                    className={cls.mainLink}
+                >
+                    {t('Главная')}
+                </AppLink>
+                <AppLink
+                    to="./about"
+                    theme={AppLinkTheme.SECONDARY}
+                >
+                    {t('О сайте')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher/ui/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((pre) => !pre);
@@ -25,7 +27,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 onClick={onToggle}
                 data-testid="sidebar-toggle"
             >
-                Toggle
+                {t('Переключатель')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
